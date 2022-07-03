11b50539567d42f859f950ef2a2f5d7f1d85b729
Deikson <deiksoncamargo@gmail.com>
2022-07-03 08:41:38 -0300

    Dev-JS-DIO

---
+++ variaveis/script.js
@@ -0,0 +1,100 @@
+// tipos primitivos
+
+// boolean
+/*var vOuF = false;
+console.log(typeof(vOuF));
+
+// number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+// string
+var nome = 'diana';
+console.log(typeof(nome));
+
+// function
+var funcao = function() {}
+console.log(typeof(funcao)); */
+
+// como declarar (sao variaveis que nao precisam de valor inicial e podem ser alteradas depois de criadas 'var' e 'let')
+/*var variavel = 'diana';
+variavel = 'martine';
+console.log(variavel);*/
+
+/* et variavel2 = 'diana';
+variavel2 = 'martine';
+console.log(variavel2);*/
+
+// esta variavel não pode ser alterada e deve ter um valor inicial obrigatoriamente
+/*const constante = 'diana'; 
+console.log(constante);*/
+
+/*var escopoGlobal = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+      let escopoGlobalInterno = 'local';
+      console.log(escopoGlobalInterno);
+   }
+
+escopoLocal();*/
+
+//atribuiçao
+// var atribuiçao = 'diana';
+
+// // comparacao
+// var comparacao = '0' == 0;
+// console.log(comparacao);
+
+// //comparacao identica
+// var comparacaoIdentica = '0' === 0;
+// console.log(comparacaoIdentica);
+
+// adicao
+// var adicao = 1 + 1;
+// console.log(adicao);
+
+// //subtracao
+// var subtracao = 2 - 1;
+// console.log(subtracao);
+
+// multiplicao
+// var multiplicao = 2 * 3;
+// console.log(multiplicao);
+
+//divisao real
+// var divisaoReal = 6 / 2;
+// console.log(divisaoReal);
+
+//divisao inteira
+// var divisaoInteira = 5 % 2;
+// console.log(divisaoInteira);
+
+//potenciacao
+// var potenciacao = 2 ** 10;
+// console.log(potenciacao);
+
+//maior que
+// var maioQue = 5 > 2;
+// console.log(maioQue);
+
+//menor que
+// var menorQue = 5 < 2;
+// console.log(menorQue);
+
+//maior ou igual a
+// var maiorOuIgual = 5 >= 2;
+// console.log(maiorOuIgual);
+
+//menor ou igual
+// var menorOuIgual = 5 <= 2;
+// console.log(menorOuIgual);
+
+var e = true && true;
+console.log(e);
+
+var ou = true || false;
+console.log(ou);
+
+var nao = !true;
+console.log(nao);
